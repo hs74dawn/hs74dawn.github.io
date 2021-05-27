bbaee75da73a6c3d0b7db081620b30fa71588c21
hanvit <gksqlcWkd!>
2021-05-28 02:50:37 +0900

    d

--- assets/js/image.js
+++ assets/js/image.js
@@ -1,14 +1,20 @@
-imgslide(); //페이지가 로딩될때 함수를 실행합니다
-function imgslide(){
-
-$val = $("#slide1").attr("val"); //현재 이미지 번호를 가져옵니다
-$mx= $("#slide1").attr("mx"); //총 이미지 개수를 가져옵니다
-$("#img"+$val).hide(); //현재 이미지를 사라지게 합니다.
-	if( $val == $mx ){ $val = 1; } //현재이미지가 마지막 번호라면 1번으로 되돌립니다.
-	else{ $val++; } //마지막 번호가 아니라면 카운트를 증가시켜줍니다
-$("#img"+$val).fadeIn(10); //변경된 번호의 이미지영역을 나타나게 합니다.괄호 안에 숫자는 페이드인 되는 시간을 나타냅니다.
-$("#slide").attr('val',$val); //변경된 이미지영역의 번호를 부여합니다.
-setTimeout('imgslide()',6000); //1초 뒤에 다시 함수를 호출합니다.(숫자값 1000당 1초)
-    }
-
-
+  var idx_lgth = $("#visual>div").length;
+  var srt = 1;
+  
+  $("section>a").click(function(){
+    var idx = $(this).index();
+    srt = idx;
+    $(this).addClass('on').siblings().removeClass('on');
+    $("#visual>div").eq(idx).addClass('on').siblings().removeClass('on');
+  });
+  
+setInterval(AutoRun, 6000);
+  
+function AutoRun(){
+  if(srt == idx_lgth){
+      srt = 0;  
+  }
+  $("section>a").eq(srt).addClass('on').siblings().removeClass('on');
+  $("#visual>div").eq(srt).addClass('on').siblings().removeClass('on');
+  srt++;  
+}
